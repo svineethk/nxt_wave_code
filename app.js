e416f9af6749b69887eb34a7231c9d0d77cd3524
svineethk <vineethkumar.selvam@gmail.com>
2023-11-12 15:57:44 +0000

    Some TestCases are not Working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+app.use(express.json());
 
 const dbPath = path.join(__dirname, "cricketTeam.db");
 let db = null;
@@ -33,5 +34,33 @@ app.get("/players/", async (request, response) => {
 app.post("/players/", async (request, response) => {
   const playerDetails = request.body;
   const { playerName, jerseyNumber, role } = playerDetails;
-  const addPlayerDetails = `Insert Into cricket_team (playerName , jerseyNumber , role) values ()`;
+  const addPlayerDetails = `INSERT INTO cricket_team (player_name , jersey_number , role) values ('${playerName}' , ${jerseyNumber} , '${role}')`;
+  const result = await db.run(addPlayerDetails);
+  const playerId = result.lastID;
+  response.send("Player Added to Team");
 });
+
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const getPlayersQuery = `SELECT * FROM CRICKET_TEAM WHERE player_id = ${playerId};`;
+  const playerList = await db.get(getPlayersQuery);
+  response.send(playerList);
+});
+
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const playerDetails = request.body;
+  const { playerName, jerseyNumber, role } = playerDetails;
+  const addPlayerDetails = `UPDATE cricket_team SET player_name = '${playerName}' , jersey_number = ${jerseyNumber} , role = '${role}' WHERE player_id = ${playerId}`;
+  await db.run(addPlayerDetails);
+  response.send("Book Updated Successfully");
+});
+
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const deletePlayerQuery = `DELETE FROM cricket_team WHERE player_id = ${playerId}`;
+  await db.run(deletePlayerQuery);
+  response.send("Player Removed");
+});
+
+module.exports = app;
